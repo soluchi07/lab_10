fdeb241d152092d29019590b1e35c997a4405198
Soluchi Fidel-Ibeabuchi <112484866+soluchi07@users.noreply.github.com>
2025-04-07 23:29:32 -0400

    commented code

--- cipher.py
+++ cipher.py
@@ -1,6 +1,7 @@
 import string
 import sys
 
+#define function for shifting characters using the key n
 def shiftChar(char, n):
     if ord(char) <= (90 - n) :
         result = chr(ord(char) + n)
@@ -8,15 +9,26 @@ def shiftChar(char, n):
         result = chr(ord(char) + n -26)
     return result
 
+#define main function
 def main():
+    #define string of uppercase letters
     letters = string.ascii_uppercase
+
+    #store stdin in uppercase as message
     message = sys.stdin.read().upper()
+
+    #take the second argument as the encryption key
     n = int(sys.argv[1])
+
+    #initialise empty string for encrypted message
     encrypted = ""
+
+    #iterate through message, shift if character if it's in the alphabet and add it to the encrypted string
     for char in message:
         if char in letters:
             encrypted += shiftChar(char, n)
-    
+
+    #iterate through new encrypted string to print characters out in 10 blocks of 5
     for i in range(len(encrypted)):
         if i % 50 == 49:
             print(encrypted[i], end = "\n")
@@ -25,6 +37,6 @@ def main():
         else:
             print(encrypted[i], end="")
         
-
+#call main function
 if __name__ == "__main__":
     main()
